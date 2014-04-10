1ecf68eeb867db272e7c0decaeaae09d8bf8954d
Dévai Nándor <nandor.devai@replise.com>
2014-04-10 10:56:50 +0200

    JShint and test server running works

--- build.sh
+++ build.sh
@@ -12,15 +12,15 @@ fi
 flake8 --exclude=.ve . > tests/reports/pep8-report.txt || true
 
 # jshint
-jshint . --reporter=jslint > tests/reports/jshint-report.txt || true
+jshint js --reporter=jslint > tests/reports/jshint-report.txt || true
 
 # unit tests
 py.test --cov tododo.py --cov-report xml --junitxml=tests/reports/pytests.xml
 mv coverage.xml tests/reports
 
 # E2E tests
-cd tests/system
 python3 tododo.py &
+cd tests/system
 protractor build.js | sed "1,6 d" | sed -e :a -e '$d;N;1ba' -e 'P;D' > ../reports/report.json; echo "]" >> ../reports/report.json
 kill %python3
 python3 json2junit.py
