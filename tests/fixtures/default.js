cfedddc3de868a9b8479ca3f7be72bbee24f6e93
Dévai Nándor <nandor.devai@replise.com>
2014-04-16 23:22:12 +0200

    archive tasks are displayed as disabled checkboxes

--- tests/fixtures/default.js
+++ tests/fixtures/default.js
@@ -8,7 +8,7 @@ var data = {
         },
         {
             text: 'Archived item',
-            completed_on: new Date(),
+            completed_on: new Date().toISODate(),
         }
     ]
 };
