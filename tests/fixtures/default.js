83babe588ca3411c1b5d84309949af9d56d20f90
Dévai Nándor <nandor.devai@replise.com>
2014-04-17 11:29:26 +0200

    default fixture ISO date fixed

--- tests/fixtures/default.js
+++ tests/fixtures/default.js
@@ -8,7 +8,7 @@ var data = {
         },
         {
             text: 'Archived item',
-            completed_on: new Date().toISODate(),
+            completed_on: new Date().toISOString(),
         }
     ]
 };
